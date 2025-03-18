3dc21932f68d452d129e93a896bfbc3e68630785
ninja9210 <pp506345@outlook.com>
2025-03-18 22:27:48 +0530

    Update WrapperExample.java

--- program 08/WrapperExample.java
+++ program 08/WrapperExample.java
@@ -2,7 +2,6 @@ public class WrapperExample {
     public static void main(String[] args) {
         int num = 100;
 
-        // Boxing - Primitive to Wrapper Object
         Integer obj = Integer.valueOf(num);
 
         System.out.println(num + " <--> " + obj);
