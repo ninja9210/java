31c2691b68610a62203d11f6b2cddff7d60d4bc6
ninja9210 <pp506345@outlook.com>
2025-03-18 22:27:32 +0530

    Update UnboxingExample.java

--- program 08/UnboxingExample.java
+++ program 08/UnboxingExample.java
@@ -2,7 +2,6 @@ public class UnboxingExample {
     public static void main(String[] args) {
         Integer obj = new Integer(100);
 
-        // Unboxing - Wrapper Object to Primitive
         int num = obj.intValue();
 
         System.out.println(num + " <--> " + obj);
